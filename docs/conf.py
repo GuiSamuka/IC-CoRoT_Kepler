181e3d6cb6ebb4e38218ab8118e50ee5d1f9bf85
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-02-25 22:32:38 -0300

    Requirements

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Filtering Techniques of Light Curves Obtained from CoRoT and Kepler satellites'
