99b32ddc7cf32fc7158c0c8cde75ceac30bf899b
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-03-26 09:22:23 -0300

    Added view_filter_results

--- tools/viz.py
+++ tools/viz.py
@@ -3,11 +3,10 @@ import plotly.graph_objs as go
 def view_lightcurve(
   x_data=None, 
   y_data=None,
-  title='Title example',
-  x_axis='x axis',
-  y_axis='y axis',
+  title='Sample Light Curve',
+  x_axis='Date',
+  y_axis='Whiteflux',
   label='label'):
-
 
   fig = go.Figure()
 
@@ -18,4 +17,29 @@ def view_lightcurve(
   fig.add_trace(go.Scatter(x=x_data, y=y_data,
                            mode='lines',
                            name=label))
+  fig.show()
+
+def view_filter_results(
+  x_original=None,
+  y_original=None,
+  x_filtered=None,
+  y_filtered=None,
+  title='Filtering result',
+  x_axis='Date',
+  y_axis='Whiteflux'
+  ):
+
+  fig = go.Figure()
+
+  fig.update_layout(title=title,
+                    xaxis_title=x_axis,
+                    yaxis_title=y_axis)
+
+  fig.add_trace(go.Scatter(x=x_original, y=y_original,
+                          mode='lines',
+                          name='Original Light Curve'))
+
+  fig.add_trace(go.Scatter(x=x_filtered, y=y_filtered,
+                          mode='lines',
+                          name='Light Curve Filtered'))
   fig.show()
