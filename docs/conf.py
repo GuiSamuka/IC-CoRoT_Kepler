365d9668aabd94f243521aaefe659e5167a69211
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-07-28 23:25:44 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,22 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# extensions = [
+#   'nbsphinx',
+#   'sphinx_rtd_theme',
+#   'sphinx.ext.autodoc',
+#   'sphinx.ext.mathjax',
+#   'sphinx_copybutton',
+#   'sphinx.ext.githubpages',
+#   'sphinx.ext.imgconverter',
+#   'sphinx.ext.doctest',
+#   'sphinx.ext.intersphinx',
+#   'sphinx.ext.todo',
+#   'sphinx.ext.coverage',
+#   'sphinx.ext.ifconfig',
+#   'sphinx.ext.viewcode',
+#   'sphinx.ext.imgmath'
+#   ]
 extensions = [
   'nbsphinx',
   'sphinx_rtd_theme',
@@ -41,15 +57,8 @@ extensions = [
   'sphinx_copybutton',
   'sphinx.ext.githubpages',
   'sphinx.ext.imgconverter',
-  'sphinx.ext.doctest',
-  'sphinx.ext.intersphinx',
-  'sphinx.ext.todo',
-  'sphinx.ext.coverage',
-  'sphinx.ext.ifconfig',
-  'sphinx.ext.viewcode',
-  'sphinx.ext.imgmath'
-  ]
-
+  'bokeh.sphinxext.bokeh_autodoc'
+]
 
 
 highlight_language = 'python3'
