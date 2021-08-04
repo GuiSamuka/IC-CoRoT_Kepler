9d57d7a16b82701bd8ad3213638dee01418b6db2
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-08-03 22:07:56 -0300

    Create fits_to_csv.py

---
+++ help_algorithms/fits_to_csv.py
@@ -0,0 +1,108 @@
+"""
+Started in: August 03, 2021
+Finished in: 
+
+@author: Guilherme Samuel
+"""
+from os import system
+system("cls")
+
+
+FITS_FILES_FOLDER_PATH = 'C:/Users/guisa/Desktop/Eclipsing_Binaries'
+
+FILE_NAME = FITS_FILES_FOLDER_PATH + '/EN2_STAR_CHR_0102582649_20071023T223035_20080303T093534.fits'
+
+from astropy.io  import fits
+import numpy as np
+import pandas as pd
+import julian
+from datetime import datetime
+import time
+import os
+import shutil
+
+def julian_to_stdtime(old_date):
+  """
+  This is the function used to convert Julian
+  date to Gregorian date
+
+  :param numpy.float64 old_date: Represents Julian date of float type
+  """
+  aux_1 = julian.from_jd(old_date, fmt='mjd')
+  aux_2 = datetime.strptime(str(aux_1), '%Y-%m-%d %H:%M:%S.%f')
+  new_date = str(aux_2)
+
+  return new_date
+
+def fits_to_csv(path, CSV_DIR):
+    '''
+    Normalize one .fits files and convert it
+    into a .csv file
+
+    :param str path: Path to .fits data-set folder
+    :"param str CSV_DIR: Path to the new folder, with the csv files
+    '''
+
+    # ``image_file`` contains all the information storage into .fits file
+    image_file = fits.open(path)
+
+    # ``scidata`` contains the information of the third table of the .fits file
+    scidata = image_file[3].data
+
+    # ``data_aux`` contains the same info as ``scidata``, but in a np.array format
+    data_aux = np.array(scidata).byteswap().newbyteorder()
+
+    # ``data`` contains the same info as ``data_aux``, but in a pd.DataFrame format
+    data = pd.DataFrame(data_aux)
+
+    # We don't need STATUSSYS column
+    data.drop('STATUSSYS', axis=1, inplace=True)
+
+    # Renaming columns
+    data.rename(columns={'DATEBARTT': 'DATE'}, inplace = True)
+    data.rename(columns={'WHITEFLUXSYS': 'WHITEFLUX'}, inplace = True)
+
+    ## Verify if there's any Not a Number (NaN) value
+    if (data.isnull().values.any()):
+        print("There's some NaN value on data. Check the data!")
+        print("Name file:", FILE_NAME)
+    
+    data.DATE = data.DATE.apply(lambda x: julian_to_stdtime(x))
+
+    # Creating folder with .csv files
+    if not os.path.isdir(CSV_DIR):
+        os.mkdir(CSV_DIR)
+    
+    # Renaming .csv files
+    name = path[path.rfind('/')+1:path.rfind('.')] + '.csv'
+
+    # Saving data
+    data.to_csv(name, index=False)
+    print("Data saved:", name)
+
+    # Move to .csv folder
+    shutil.move(name, CSV_DIR)
+
+def main():
+    """
+    Function that apply the `fits_to_csv`
+    to all .fits dataset (FITS_FILES_FOLDER_PATH)
+    """
+    i = 0
+    my_dir = FITS_FILES_FOLDER_PATH
+    t0 = time.time()
+
+    for root_dir_path, sub_dirs, files in os.walk(my_dir):
+        for i in range(0, len(files)):
+            path = my_dir + '/' + files[i] 
+
+            if path.endswith('.fits'):
+                fits_to_csv(path, "C:/Users/guisa/Desktop/eclipsing_binaries_csv")
+                break
+            
+
+    tf = time.time()
+    print("\nIt takes:", round(tf-t0, 2), "seconds to apply")
+
+if __name__ == '__main__':
+    main()
