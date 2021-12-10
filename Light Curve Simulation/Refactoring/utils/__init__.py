19188aad693050b9d6fbf6f05969620402136d77
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-12-10 18:46:43 -0300

    Fixing repo

--- Light Curve Simulation/Refactoring/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import lightcurve
-from . import data_viz
-from . import data_helper
-from . import filter_helper
-import logging
-
-log = logging.getLogger(__name__)
-log.addHandler(logging.StreamHandler())
--- Light Curve Simulation/utils/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .filteredlightcurve import *
-from .lightcurve import *
-from .phasefolded import *
-from .simulation import *
